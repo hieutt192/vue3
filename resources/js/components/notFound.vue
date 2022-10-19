2e5c59ff4ccc4553cd670e2a9ec33e3aa95a97cf
your name <hieutt192@gmail.com>
2022-10-19 08:03:15 +0700

    create login logout admin

---
+++ resources/js/components/notFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Not Found page</h1>
+    </div>
+</template>
---
+++ resources/js/router/index.js
@@ -0,0 +1,72 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+//admin
+import homeAdminIndex from '../components/admin/home/index.vue'
+//page
+import homePageIndex from '../components/pages/home/index.vue'
+//login
+import login from '../components/auth/login.vue'
+
+import notFound from '../components/notFound.vue'
+
+
+const routes = [
+    //admin
+    {
+        path: '/admin/home',
+        name: 'adminHome',
+        component: homeAdminIndex,
+        meta:{
+            requiresAuth:true
+        }
+    },
+
+    //Pages
+    {
+        path: '/',
+        name: 'Home',
+        component: homePageIndex,
+        meta:{
+            requiresAuth:false
+        }
+    },
+
+    //login
+    {
+        path: '/login',
+        name: 'Login',
+        component: login,
+        meta:{
+            requiresAuth:false
+        }
+    },
+
+    // not Found
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'notFound',
+        component: notFound,
+        meta:{
+            requiresAuth:false
+        }
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+router.beforeEach((to,from)=>{
+    if (to.meta.requiresAuth && !localStorage.getItem('token')) {
+        return { name: 'Login'}
+    }
+
+    if (to.meta.requiresAuth == false && localStorage.getItem('token')) {
+        return { name: 'adminHome'}
+    }
+
+    
+})
+
+export default router
